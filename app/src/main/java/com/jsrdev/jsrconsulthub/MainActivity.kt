08206c0ca6c551d1243bd7c319fe70ff7bed74cf
Jose SR <josesanchez09190@gmail.com>
2023-10-05 22:05:11 -0600

    Add proyect

---
+++ app/src/main/java/com/jsrdev/jsrconsulthub/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jsrdev.jsrconsulthub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
