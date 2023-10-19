9da1f9ad5f880c7e98702a521bdcaeb3aa3f0362
Jose SR <josesanchez09190@gmail.com>
2023-10-18 22:35:54 -0600

    added dynamic token

--- app/src/main/java/com/jsrdev/jsrconsulthub/core/Constants.kt
+++ app/src/main/java/com/jsrdev/jsrconsulthub/core/Constants.kt
@@ -3,5 +3,5 @@ package com.jsrdev.jsrconsulthub.core
 object Constants {
     const val BASE_URL_CONSULT_HUB = "https://94d0-189-217-51-223.ngrok-free.app/"
     const val BASE_URL_POSTAL_CODE = "https://api.copomex.com/query/"
-    const val TEMP_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJKb3NlU1IiLCJpc3MiOiJqc3JfZGV2IiwiaWQiOjEsImV4cCI6MTY5NzQ5NjA1M30.KhgfVxEnndlKQGpexCVPJzfsLcLHNcI_yN_CwXst89I"
+    const val TOKEN = "ey"
 }
